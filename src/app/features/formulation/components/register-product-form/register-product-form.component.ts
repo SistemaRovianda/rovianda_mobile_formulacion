d5a86e8167267215cc43642a16bc77a52091aa2d
Damian Zamora <desarrollo8@copsis.com>
2020-09-06 01:08:41 -0500

    Aceptando negativos en temperatura

--- src/app/features/formulation/components/register-product-form/register-product-form.component.ts
+++ src/app/features/formulation/components/register-product-form/register-product-form.component.ts
@@ -8,27 +8,15 @@ import {
 } from "@angular/forms";
 import { AppStateInterface } from "src/app/shared/models/storeState.interface";
 import { Store, select } from "@ngrx/store";
-import { Product } from "src/app/shared/models/product.interface";
 import { Observable, from } from "rxjs";
-import { SELECT_PRODUCTS } from "../../store/products/products.selectors";
-//import { SELECT_CATALOG_LOTS } from "../../store/catalogLots/catalogLots.selectors";
-import { loadIngredientsByProductID } from "../../store/ingredients-product/ingredients-product.actions";
-import * as fromActionsCatalogLots from "../../store/catalogLots/catalogLots.actions";
-import { SELECT_INGREDIENTS_BY_PRODUCT_LOADING } from "../../store/ingredients-product/ingredients-product.selectors";
 import { ModalController } from "@ionic/angular";
 import { AddIngredientComponent } from "../../dialogs/add-ingredient/add-ingredient.component";
-import { loadIngredientsOutlet } from "../../store/ingredients-outlet/ingredients-outlet.actions";
-import { SELECT_INGREDIENTS_CHECKED } from "../../store/ingredients/ingredients.selectors";
-import { IngredientC } from "src/app/shared/models/ingredient.interface";
-import { Lot, catalogLots } from "src/app/shared/models/lot.interface";
+import { Lot } from "src/app/shared/models/lot.interface";
 //import { SELECT_LOTS } from "../../store/lots/lots.selectors";
 import { noWhiteSpace } from "src/app/shared/validators/white-space.validator";
-import { textValidator } from "src/app/shared/validators/text.validator";
 import { SELECT_FORMULARION_REGISTER_SAVE } from "../../store/register-formulation/register-formulation.selector";
 import { registerNewRegistration } from "../../store/register-formulation/register-formulation.action";
 import * as moment from "moment";
-import { UserVerified } from "src/app/shared/models/user.interface";
-import { usersVerifiedSelector } from "../../store/users-verified/users-verified.selector";
 import { Storage } from "@ionic/storage";
 import { productsRovianda } from '../../store/productsRovianda/reducer';
 import { GET_ALL_PRODUCTS_ROVIANDA, GET_INGREDIENTS_PRODUCT_ROVIANDA } from '../../store/productsRovianda/actions';
@@ -42,12 +30,9 @@ import { GET_QUALITY_USERS_STORE } from '../../store/quality-user/selectors';
 import { ingredientsOfProductRovianda } from '../../store/ingredients-product-rovianda/reducer';
 import { GET_INGREDIENTS_PRODUCT_ROVIANDA_STORE } from '../../store/ingredients-product-rovianda/selector';
 import { lotsDrief } from '../../store/lotsDrief/reducer';
-
 import { GET_ALL_INGREDIENTS_AVAILABLES, SET_INGREDIENTS } from '../../store/ingredients-product-rovianda/actions';
-
 import { SELECT_USER_UID } from 'src/app/features/landing/store/authentication/authentication.selectors';
 import { GET_LOTS_DRIEF_STORE } from '../../store/lotsDrief/selectors';
-
 
 @Component({
   selector: "register-product-form",
@@ -55,28 +40,20 @@ import { GET_LOTS_DRIEF_STORE } from '../../store/lotsDrief/selectors';
   styleUrls: ["./register-product-form.component.scss"],
 })
 export class RegisterProductFormComponent implements OnInit {
+
   form: FormGroup;
-
   loadingIngredients: boolean;
-
   productsRovianda$: Observable<productsRovianda[]>;
-  ingredientsOfProductRovianda$:Observable<ingredientsOfProductRovianda[]>;
+  ingredientsOfProductRovianda$: Observable<ingredientsOfProductRovianda[]>;
   lotsMeat$: Observable<OutputsMeat[]>;
   lotsDrief$: Observable<lotsDrief[]>;
-
-
   lots$: Observable<Lot[]>;
-
   qualityUsers$: Observable<qualityUser[]>;
-
   nameElaborated: Observable<string>;
-
   uidElaborated: Observable<String>;
-
   lotsFormArray: FormArray = new FormArray([]);
-  userId:string;
-
-  ingredients:ingredientsOfProductRovianda[];
+  userId: string;
+  ingredients: ingredientsOfProductRovianda[];
 
   @Output("onSubmit") submit = new EventEmitter();
 
@@ -87,14 +64,14 @@ export class RegisterProductFormComponent implements OnInit {
     private _storage: Storage
   ) {
     this.loadingIngredients = true;
-    
+
 
     //this.catalogLots$ = this._store.pipe(select(SELECT_CATALOG_LOTS));
 
     this.form = fb.group({
       productRoviandaId: ["", [Validators.required]],
       lotIdRecordId: ["", [Validators.required, noWhiteSpace]],
-      temperature: ["", [Validators.required, noWhiteSpace, textValidator]],
+      temperature: ["", [Validators.required, noWhiteSpace]],
       temperatureWater: ["", [Validators.required, noWhiteSpace]],
       date: [
         { value: moment(new Date()).format("DD/MM/YYYY"), disabled: true },
@@ -116,7 +93,7 @@ export class RegisterProductFormComponent implements OnInit {
     this.ingredientsOfProductRovianda$ = this._store.pipe(select(GET_INGREDIENTS_PRODUCT_ROVIANDA_STORE));
     this.lotsDrief$ = this._store.pipe(select(GET_LOTS_DRIEF_STORE))
     this.ingredientsOfProductRovianda$.subscribe((lots) => {
-      this.ingredients=lots;
+      this.ingredients = lots;
       this.createLotsFormArray(lots.length);
     });
     this._store.select(SELECT_FORMULARION_REGISTER_SAVE).subscribe((res) => {
@@ -124,14 +101,14 @@ export class RegisterProductFormComponent implements OnInit {
         this.form.reset();
         this.lotsFormArray = new FormArray([]);
         this._store.dispatch(registerNewRegistration());
-        this._store.dispatch(SET_INGREDIENTS({ingredients:[]}));
+        this._store.dispatch(SET_INGREDIENTS({ ingredients: [] }));
         this._store.dispatch(GET_ALL_PRODUCTS_ROVIANDA());
         this._store.dispatch(GET_ALL_OUTPUTS_MEAT());
         this._store.dispatch(GET_ALL_QUALITY_USERS());
         this._store.dispatch(GET_ALL_INGREDIENTS_AVAILABLES());
       }
     });
-    
+
     // this._store
     //   .select(SELECT_INGREDIENTS_BY_PRODUCT_LOADING)
     //   .subscribe((loading) => {
@@ -145,8 +122,8 @@ export class RegisterProductFormComponent implements OnInit {
         GET_INGREDIENTS_PRODUCT_ROVIANDA({ productId })
       );
     });
-    this._store.pipe(select(SELECT_USER_UID)).subscribe((uid)=>{
-      this.userId=uid;
+    this._store.pipe(select(SELECT_USER_UID)).subscribe((uid) => {
+      this.userId = uid;
     });
 
     // //this.lots$ = this._store.select(SELECT_LOTS);
@@ -184,16 +161,16 @@ export class RegisterProductFormComponent implements OnInit {
     this.submit.emit(f);
   }
 
-  getLotsIdWithIngredientsId(values:any){
-    console.log("Values",values);
-    return values.map(((x,index)=>{
+  getLotsIdWithIngredientsId(values: any) {
+    console.log("Values", values);
+    return values.map(((x, index) => {
       return {
-        lotRecordId:x,
-        ingredientId:this.ingredients[index].productId
+        lotRecordId: x,
+        ingredientId: this.ingredients[index].productId
       }
     }))
   }
-  
+
 
   async openModal() {
     const modal = await this._modalCtrl.create({
@@ -211,7 +188,7 @@ export class RegisterProductFormComponent implements OnInit {
     }
   }
 
-  
+
 
   get productRoviandaId() {
     return this.form.get("productRoviandaId");
